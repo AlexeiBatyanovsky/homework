69c04c8f729be085639518ece200576eafd1fa35
Alex <massdefect93@gmail.com>
2024-05-03 14:01:04 +0300

    up3

--- lesson7/1.py
+++ lesson7/1.py
@@ -6,8 +6,11 @@ n = 0
 while bal != 0:
     n = n + 1
     bal = int(input('Введите оценку: '))
-    sum = sum + bal
-    sr_bal = sum/(n)
-    print(n, bal, sr_bal, sum)
+    if bal != 0:
+        sum = sum + bal
+        sr_bal = sum/(n)
+        print(n, bal, sr_bal, sum)
+    else:
+        break
 else:
     print('средний балл: ', sr_bal)
