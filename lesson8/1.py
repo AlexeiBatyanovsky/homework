eca0de45b60b154d9d5b2645f6a3a23d49a6f05c
Alex <massdefect93@gmail.com>
2024-05-06 17:00:48 +0300

    up

--- lesson8/1.py
+++ lesson8/1.py
@@ -1,10 +1,10 @@
-def fio(a, b = True):
+def funct(a, b):
     if b:
         print(f'- {a[0]} {a[1][0]}.{a[2][0]}.')
     else:
         print(f"- {a[1][0]}.{a[2][0]}.{a[0]}")
     
-a = input('Enter fio: ').split()
-b = int(input('Enter param: '))
+fio = input('Enter FIO: ').split()
+param = int(input('Enter param: '))
 
-print(fio(a, b))
+print(funct(fio, param))
